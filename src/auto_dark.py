e77864014a82785b4ee8d429fabeae2880f0ed3c
Johannes Rappen <jrappen@outlook.com>
2020-03-11 08:35:04 +0100

    fix(Python): Don't use a class

--- src/auto_dark.py
+++ src/auto_dark.py
@@ -6,112 +6,105 @@ import sublime
 import sublime_plugin
 
 
+running = False
+
+
 def plugin_loaded():
-    AutoDark.start()
+    _start()
 
 
 def plugin_unloaded():
-    AutoDark.stop()
+    _stop()
+
+
+def _has_dark_mode_support():
+    pf = sublime.platform()
+    if pf == 'osx':
+        from distutils.version import LooseVersion as V
+        import platform
+        if V(platform.mac_ver()[0]) < V('10.14'):
+            return False
+    elif pf == 'linux':
+        return False
+    return True
+
+
+def _start():
+    if not _has_dark_mode_support():
+        return
+    running = True
+    _tick()
+
+
+def _stop():
+    running = False
+
+
+def _tick():
+    try:
+        if running:
+            sublime.set_timeout_async(_tick, _update())
+    except Exception as e:
+        print('AutoDark: Error: ', e)
+        _stop()
+
+
+def _update():
+    import datetime
+    now = datetime.datetime.now()
+    _paint()
+    # every 5 mins max
+    return 60000 * (4 - now.minute % 5) + 1000 * (60 - now.second)
+
+
+def _paint():
+    try:
+        pref = sublime.load_settings('Preferences.sublime-settings')
+        is_dark = _is_dark_os()
+        cs_now = pref.get('color_scheme', 'Monokai.sublime-color-scheme')
+        cs_new = pref.get('color_scheme.dark', 'Mariana.sublime-color-scheme') if is_dark else pref.get('color_scheme.light', 'Breakers.sublime-color-scheme')
+        if cs_now != cs_new:
+            pref.set('color_scheme', cs_new)
+            sublime.save_settings('Preferences.sublime-settings')
+    except Exception as e:
+        print('AutoDark: Error: ', e)
+
+
+def _is_dark_os():
+    pf = sublime.platform()
+    if pf == 'osx':
+        try:
+            import subprocess
+            status = subprocess.check_output(
+                'defaults read -g AppleInterfaceStyle'.split(),
+                stderr = subprocess.STDOUT
+            ).decode().replace('\n', '')
+        except Exception as e:
+            return False
+        return True if status.lower() == 'dark' else False
+    elif pf == 'windows':
+        value = 1 # default to light theme
+        try:
+            import winreg
+            with winreg.ConnectRegistry(
+                None,
+                winreg.HKEY_CURRENT_USER
+            ) as registry:
+                with winreg.OpenKey(
+                    registry,
+                    'SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize'
+                ) as key:
+                    value, value_type = winreg.QueryValueEx(key, 'AppsUseLightTheme')
+        except Exception as e:
+            print('AutoDark: Error: ', e)
+        return not bool(value)
+    else:
+        return False
 
 
 class EventListener(sublime_plugin.EventListener):
 
     def on_activated_async(self, view):
-        AutoDark.paint()
-
-
-class AutoDark(object):
-
-    running = False
-
-    @classmethod
-    def has_dark_mode_support(cls):
-        current_pf = sublime.platform()
-        if current_pf == 'osx':
-            from distutils.version import LooseVersion as V
-            import platform
-            if V(platform.mac_ver()[0]) < V('10.14'):
-                return False
-        elif current_pf == 'linux':
-            return False
-        return True
-
-    @classmethod
-    def start(cls):
-        if not cls.has_dark_mode_support():
+        if not _has_dark_mode_support():
             return
-        cls.running = True
-        cls._tick()
-
-    @classmethod
-    def stop(cls):
-        cls.running = False
-
-    @classmethod
-    def _tick(cls):
-        try:
-            if cls.running:
-                sublime.set_timeout_async(cls._tick, cls._update())
-        except Exception as e:
-            print('AutoDark: Error: ', e)
-            cls.stop()
-
-    @classmethod
-    def _update(cls):
-        import datetime
-        now = datetime.datetime.now()
-        try:
-            cls.paint()
-        except Exception as e:
-            print('AutoDark: Error: ', e)
-        # every 5 mins max
-        return 60000 * (4 - now.minute % 5) + 1000 * (60 - now.second)
-
-    @classmethod
-    def paint(cls):
-        try:
-            PREF = sublime.load_settings('Preferences.sublime-settings')
-            current_pf = sublime.platform()
-            IS_DARK = False if current_pf == 'linux' else cls.is_dark_os()
-            CS_NOW = PREF.get('color_scheme', 'Monokai.sublime-color-scheme')
-            CS_NEW = PREF.get('color_scheme.dark', 'Mariana.sublime-color-scheme') if IS_DARK else PREF.get('color_scheme.light', 'Breakers.sublime-color-scheme')
-            if CS_NOW != CS_NEW:
-                PREF.set('color_scheme', CS_NEW)
-                sublime.save_settings('Preferences.sublime-settings')
-        except Exception as e:
-            print('AutoDark: Error: ', e)
-
-    @classmethod
-    def is_dark_os(cls):
-        current_pf = sublime.platform()
-        if current_pf == 'osx':
-            try:
-                get_status = 'defaults read -g AppleInterfaceStyle'
-                import subprocess
-                status = subprocess.check_output(
-                    get_status.split(),
-                    stderr = subprocess.STDOUT
-                ).decode()
-                status = status.replace('\n', '')
-            except Exception as e:
-                print('AutoDark: Error: ', e)
-                return False
-            return True if status.lower() == 'dark' else False
-        elif current_pf == 'windows':
-            value = 1 # default to light theme
-            try:
-                import winreg
-                with winreg.ConnectRegistry(
-                    None,
-                    winreg.HKEY_CURRENT_USER
-                ) as registry:
-                    with winreg.OpenKey(
-                        registry,
-                        'SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize'
-                    ) as key:
-                        value, value_type = winreg.QueryValueEx(key, 'AppsUseLightTheme')
-            except Exception as e:
-                print('AutoDark: Error: ', e)
-            return not bool(value)
-        else:
-            return False
+        _paint()
